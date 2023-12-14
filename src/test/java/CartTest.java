9115b3cdcd9dfc680fad278184d7834e95fb80ea
Omar Zeroual <omar.zeroual@chanel.com>
2023-12-14 15:52:58 +0100

    ad coverage

--- src/test/java/CartTest.java
+++ src/test/java/CartTest.java
@@ -1,7 +1,6 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 class CartTest {
 
@@ -19,5 +18,16 @@ class CartTest {
         cart.addProduct(product1);
         double totalPrice = cart.getTotalPrice();
         Assertions.assertEquals(10.5, totalPrice);
+    }
+    @Test
+    void getTotalPrice_WhenCardContainsBooksProductsOnly_ShouldReturnSumOfProductsMultipliedByBooksCoefficient() {
+        double productPrice = 10;
+        double genericCoefficient = 1.05;
+        double booksCoefficient = 0.9;
+        Cart cart = new Cart();
+        Product product1 =  Product.builder().isBook(true).price(10).stock(1).build();
+        cart.addProduct(product1);
+        double totalPrice = cart.getTotalPrice();
+        Assertions.assertEquals(productPrice * genericCoefficient * booksCoefficient, totalPrice);
     }
 }
