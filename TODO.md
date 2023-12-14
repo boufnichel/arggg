f854b4c79b221883834e9630ef5775f21be75efc
boufnichel <boufnichel.mohamed@gmail.com>
2023-12-14 16:09:07 +0100

    rename test methods

--- TODO.md
+++ TODO.md
@@ -1,4 +1,4 @@
-- add the TEST for the case where the card contains books products only
-- rename tests methods
+~~- add the TEST for the case where the card contains books products only~~
+~~- rename tests methods~~
 - Refactoring
 - implement the needed requirements 
--- src/test/java/CartTest.java
+++ src/test/java/CartTest.java
@@ -1,18 +1,21 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 
 class CartTest {
 
     @Test
-    void getTotalPriceWhenProductsEmpty() {
+    void price_should_zero_when_the_bag_is_empty() {
         Cart cart = new Cart();
         double totalPrice = cart.getTotalPrice();
         Assertions.assertEquals(0, totalPrice);
     }
 
     @Test
-    void getTotalPrice_WhenProductsNotEmptyAndNotBookNorSpacial_ShouldReturnSumOfProductsPrices() {
+    void normal_price_should_be_applied_when_no_book_is_ordered() {
+
         Cart cart = new Cart();
         Product product1 =  Product.builder().price(10).stock(1).build();
         cart.addProduct(product1);
@@ -20,7 +23,7 @@ class CartTest {
         Assertions.assertEquals(10.5, totalPrice);
     }
     @Test
-    void getTotalPrice_WhenCardContainsBooksProductsOnly_ShouldReturnSumOfProductsMultipliedByBooksCoefficient() {
+    void discounted_price_should_be_applied_when_a_book_is_ordered() {
         double productPrice = 10;
         double genericCoefficient = 1.05;
         double booksCoefficient = 0.9;
